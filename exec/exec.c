24918c08764ac4344f5660945c06ef0dbc0d8a74
Fernanda Eschallots <feschall@oa-h3.msk.21-school.ru>
2021-10-13 08:01:49 +0300

    Final 2.0!

--- exec/exec.c
+++ exec/exec.c
@@ -6,19 +6,11 @@
 /*   By: eerika <eerika@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 17:25:27 by eerika            #+#    #+#             */
-/*   Updated: 2021/10/12 12:07:41 by eerika           ###   ########.fr       */
+/*   Updated: 2021/10/12 13:50:18 by eerika           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-void	free_mem(char **arr, char *str)
-{
-	if (arr)
-		free_arr(arr);
-	if (str)
-		free(str);
-}
 
 int	get_pipe_number(t_cmds *cmd)
 {
@@ -55,38 +47,40 @@ static void	sig_proc(t_cmds *cmd)
 	}
 }
 
-void	execution(t_cmds *cmd, char ***env)
+static int	handle_path(t_cmds *cmd, char ***env, char ***path, char **tmp)
 {
-	char	**path;
-	char	*tmp;
+	int	isbuildin;
+
+	isbuildin = 0;
+	if (cmd->args[0])
+	{
+		isbuildin = check_buildin(cmd->args[0]);
+		if (!isbuildin && (access(cmd->args[0], X_OK)))
+		{
+			*path = get_path(*env);
+			if (!*path)
+			{
+				error_cmd_not_found(cmd->args[0]);
+				free_mem(*path, *tmp);
+				return (isbuildin);
+			}
+			*tmp = cmd->args[0];
+			cmd->args[0] = test_path(*path, cmd->args[0]);
+		}
+	}
+	return (isbuildin);
+}
+
+static void	common_exec(t_cmds *cmd, char ***env, char ***path, char **tmp)
+{
 	int		in_out[2];
 	int		isbuildin;
 	int		stdin_initial;
-	t_cmds	*head;
 
-	head = cmd;
 	stdin_initial = dup(STDIN_FILENO);
-	tmp = NULL;
-	path = NULL;
-	isbuildin = 0;
 	while (cmd != NULL)
 	{
-		if (cmd->args[0])
-		{
-			isbuildin = check_buildin(cmd->args[0]);
-			if (!isbuildin && (access(cmd->args[0], X_OK)))
-			{
-				path = get_path(*env);
-				if (!path)
-				{
-					error_cmd_not_found(cmd->args[0]);
-					free_mem(path, tmp);
-					break ;
-				}
-				tmp = cmd->args[0];
-				cmd->args[0] = test_path(path, cmd->args[0]);
-			}
-		}
+		isbuildin = handle_path(cmd, env, path, tmp);
 		open_files(cmd, &in_out[0], &in_out[1]);
 		if (isbuildin)
 			exec_buildins(cmd, in_out[1], env, stdin_initial);
@@ -100,8 +94,20 @@ void	execution(t_cmds *cmd, char ***env)
 				exec_cmd(cmd, isbuildin, env);
 		}
 		if (!isbuildin)
-			free_mem(path, tmp);
+			free_mem(*path, *tmp);
 		cmd = cmd->next;
 	}
+}
+
+void	execution(t_cmds *cmd, char ***env)
+{
+	char	**path;
+	char	*tmp;
+	t_cmds	*head;
+
+	head = cmd;
+	tmp = NULL;
+	path = NULL;
+	common_exec(cmd, env, &path, &tmp);
 	sig_proc(head);
 }
